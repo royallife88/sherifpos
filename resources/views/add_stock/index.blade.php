956764cf339923cb77dc423f11a9979ec15ceb77
royallife88 <devdeb@outlook.com>
2022-02-05 08:55:23 +0500

    tax to expenses final cost for add stock

--- resources/views/add_stock/index.blade.php
+++ resources/views/add_stock/index.blade.php
@@ -214,28 +214,7 @@
         })
     });
 
-    pdfMake.fonts = {
-        arabic: {
-            normal: 'http://127.0.0.1:8000/fonts/ar/LateefRegOT.ttf',
-            // bold: 'https://example.com/fonts/fontFile2.ttf',
-            // italics: 'https://example.com/fonts/fontFile3.ttf',
-            // bolditalics: 'https://example.com/fonts/fontFile4.ttf'
-        },
 
-        // download default Roboto font from cdnjs.com
-        Roboto: {
-            normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
-            bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
-            italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
-            bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
-        },
-
-        // example of usage fonts in collection
-        // PingFangSC: {
-        //     normal: ['https://example.com/fonts/pingfang.ttc', 'PingFangSC-Regular'],
-        //     bold: ['https://example.com/fonts/pingfang.ttc', 'PingFangSC-Semibold'],
-        // }
-    }
 
     $('.time_picker').focusout(function (event) {
         add_stock_table.ajax.reload();
