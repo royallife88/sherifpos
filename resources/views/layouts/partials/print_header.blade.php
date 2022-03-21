c9a64e2fe5ff600085d15a2d6e3a25bd0f1484d9
royallife88 <devdeb@outlook.com>
2022-03-21 17:57:31 +0500

    2342432

--- resources/views/layouts/partials/print_header.blade.php
+++ resources/views/layouts/partials/print_header.blade.php
@@ -2,5 +2,5 @@
     @php
         $letter_header = App\Models\System::getProperty('letter_header');
     @endphp
-    <img src="@if(!empty($letter_header)){{asset('uploads/'.$letter_header)}}@else{{asset('/uploads/'.session('logo'))}}@endif" alt="header" id="header_invoice_img" style="width: auto; margin: auto; max-height: 150px;">
+    <img src="@if(!empty($letter_header)){{asset('uploads/'.$letter_header)}}@else{{asset('/uploads/'.session('logo'))}}@endif" alt="header" id="header_invoice_img" style="width: auto; margin: auto; height: 150px;  max-height: 150px;">
 </div>
