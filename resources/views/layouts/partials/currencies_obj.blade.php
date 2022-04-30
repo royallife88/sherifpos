6455b917c7abc74775193149fcfb50c42331c8ac
royallife88 <devdeb@outlook.com>
2022-04-30 18:55:01 +0500

    update 6

--- resources/views/layouts/partials/currencies_obj.blade.php
+++ resources/views/layouts/partials/currencies_obj.blade.php
@@ -6,7 +6,7 @@ $currencies_obj = App\Models\ExchangeRate::leftjoin('currencies', 'exchange_rate
     ->select('received_currency_id as currency_id', 'currencies.symbol', 'conversion_rate')
     ->get();
 
-$currencies_obj = $currencies_obj->count() > 0 ? $currencies_obj->toArray() : [];
+$currencies_obj = !empty($currencies_obj) && $currencies_obj->count() > 0 ? $currencies_obj->toArray() : [];
 
 $default_currency_id = App\Models\System::getProperty('currency');
 $default_currency = App\Models\Currency::where('id', $default_currency_id)
